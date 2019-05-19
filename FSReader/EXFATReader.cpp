bb0a02ddcf49e8abe99d110a5b90e9cd6cdbeb12
egolovin1996 <egolovin1996@gmail.com>
2019-05-19 23:58:58 +0500

    #10 Добавил exFAT

--- FSReader/EXFATReader.cpp
+++ FSReader/EXFATReader.cpp
@@ -5,10 +5,16 @@ typedef struct
 {
 	BYTE Padding1[3];
 	BYTE OEM_Name[8];
-	BYTE BytesPerSector[2];
-	BYTE SectorsPerCluster[1];
-	BYTE Padding2[23];
-	long TotalSectors;
+	BYTE Padding2[61];
+	ULONGLONG TotalSectors;
+	DWORD FATStartSector;
+	DWORD FATSizeInSectors;
+	DWORD FirstDataSector;
+	DWORD TotalClusters;
+	DWORD RootDirCluster;
+	BYTE Padding3[8];
+	BYTE SectorFactor;
+	BYTE ClusterFactor;
 
 } EXFATBootRecord;
 
@@ -16,8 +22,8 @@ EXFATReader::EXFATReader(FileReader* fileReader) : BaseReader(fileReader)
 {
 	EXFATBootRecord *exfatBootRecord = (EXFATBootRecord*)_FileDataReader->ReadData(0, 1024);
 
-	_BytesPerSector = *(unsigned short*)exfatBootRecord->BytesPerSector;
-	int sectorsPerCluster = *(unsigned short*)exfatBootRecord->SectorsPerCluster;
+	_BytesPerSector = pow(2, exfatBootRecord->SectorFactor);
+	int sectorsPerCluster = pow(2, exfatBootRecord->ClusterFactor);
 	_BytesPerCluster = _BytesPerSector * sectorsPerCluster;
 	_TotalSectors = exfatBootRecord->TotalSectors;
 	_TotalClusters = _TotalSectors / sectorsPerCluster;
